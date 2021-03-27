c1974c5d4035dc9bd170f2ca8a9adbfbb25bb9cc
Alexa Carrington <acarring@wellesley.edu>
2021-03-27 12:05:03 -0400

    Edited styling in scss, linked Google Font in html

--- app.js
+++ app.js
@@ -21,14 +21,12 @@ function searchLyrics() {
     }
     if (artistInput !== "" && songInput !== "") {
         
-
         let regex = /\s+|_+/g;
         let artist = artistInput.replace(regex, "-");
         let song = songInput.replace(regex, "-");
         fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 let lyrics = data.lyrics;
                 lyricsCont.innerHTML += lyrics.replace(/([A-Z]+)/g, ",$1").replace(/,/g," <br />").split(",");
             });
