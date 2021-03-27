bf393416cfaaebc7e1b3102eaadfe36e26f720c5
Alexa Carrington <acarring@wellesley.edu>
2021-03-27 11:35:13 -0400

    Added selectors, searchLyrics function, event listener in js

---
+++ app.js
@@ -0,0 +1,40 @@
+const artist = document.querySelector('.fields .artist');
+const song = document.querySelector('.fields .song');
+const searchBtn = document.querySelector('.fields .search');
+const lyricsCont = document.querySelector('.lyrics');
+
+
+function searchLyrics() {
+    //clear lyrics container
+    lyricsCont.textContent = "";
+
+
+    let artistInput = artist.value;
+    let songInput = song.value;
+
+    //return alert if either field is left empty
+    if (artistInput === "" || songInput === "") {
+        alert("Enter both an artist and a song to search for lyrics.")
+      
+    }
+    if (artistInput !== "" && songInput !== "") {
+        
+
+        let regex = /\s+|_+/g;
+        let artist = artistInput.replace(regex, "-");
+        let song = songInput.replace(regex, "-");
+        fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                let lyrics = data.lyrics;
+                lyricsCont.innerHTML += lyrics.replace(/([A-Z]+)/g, ",$1").replace(/,/g," <br />").split(",");
+            });
+    }
+
+    //clear both fields after search button is pressed
+    artist.value = "";
+    song.value = "";
+}
+
+searchBtn.addEventListener("click", searchLyrics)
